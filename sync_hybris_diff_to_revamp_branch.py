6e61f4207c3b681e02f2106feeb1791e1aff187e
TW - IT - BE - Willy Cheng <willy.cheng@shoalter.com>
2024-12-18 10:18:21 +0800

    copy result after generate hybris diff report

--- sync_hybris_diff_to_revamp_branch.py
+++ sync_hybris_diff_to_revamp_branch.py
@@ -1,6 +1,8 @@
 from service.sync_hybris_diff_to_revamp_svc import SyncHybrisDiffToRevampService
+import pyperclip
 
 if __name__ == '__main__':
     sync_hybris_diff_to_revamp_svc = SyncHybrisDiffToRevampService("E:/tmp/hktv-hybris")
-    output_json = sync_hybris_diff_to_revamp_svc.get_branch_change_summary("feature/HYBRIS-3718", "87653fcf5c212749e4424b3e3bc915edf06502e8")
+    output_json = sync_hybris_diff_to_revamp_svc.get_branch_change_summary("feature/HYBRIS-3714", "1d8fb1dd511bb122cafe96182750ce76b8a04f1a")
+    pyperclip.copy(output_json)
     print(output_json)
--- sync_hybris_diff_to_revamp_release.py
+++ sync_hybris_diff_to_revamp_release.py
@@ -1,6 +1,9 @@
+import pyperclip
+
 from service.sync_hybris_diff_to_revamp_svc import SyncHybrisDiffToRevampService
 
 if __name__ == '__main__':
     sync_hybris_diff_to_revamp_svc = SyncHybrisDiffToRevampService("E:/tmp/hktv-hybris")
-    output_json = sync_hybris_diff_to_revamp_svc.get_release_change_summary("origin/release/20241104_0545")
+    output_json = sync_hybris_diff_to_revamp_svc.get_release_change_summary("origin/release/20241216_0545")
+    pyperclip.copy(output_json)
     print(output_json)
